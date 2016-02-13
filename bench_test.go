f214add767b649c7652ac532b5b23f33ebdd3332
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-02-13 19:00:15 +0800

    added benchmark

---
+++ bench_test.go
@@ -0,0 +1,33 @@
+package expr
+
+import "testing"
+
+func BenchmarkExprParse(b *testing.B) {
+	env := map[string]Var{}
+	funcs := map[string]Func{
+		"plusone": NewFunc(func(args FuncArgs, env FuncEnv) Num {
+			return args[0].Eval() + 1
+		}),
+	}
+	expr := "x=2+3*(x/(42+plusone(x))),x"
+	for i := 0; i < b.N; i++ {
+		Parse(expr, env, funcs)
+	}
+}
+
+func BenchmarkExprEval(b *testing.B) {
+	env := map[string]Var{}
+	funcs := map[string]Func{
+		"plusone": NewFunc(func(args FuncArgs, env FuncEnv) Num {
+			return args[0].Eval() + 1
+		}),
+	}
+	expr := "x=2+3*(x/(42+plusone(x))),x"
+	if e, err := Parse(expr, env, funcs); err != nil {
+		b.Error(err)
+	} else {
+		for i := 0; i < b.N; i++ {
+			e.Eval()
+		}
+	}
+}
